02dfce0cd295629710d189c28239ec97c2690363
Erik Malo Schuessler <90277878+Brainspeck@users.noreply.github.com>
2025-12-04 06:37:28 +0000

    added the add and delete features

--- src/js/components/ToDo.jsx
+++ src/js/components/ToDo.jsx
@@ -6,17 +6,38 @@ import "../../styles/ToDo.css";
 //create your first component
 const ToDo = () => {
 	const [inputValue, setInputValue] = useState("");
+	const [toDos, setToDos] = useState([]);
+	
+	const handleOnChange = (e) => {
+		setInputValue(e.target.value);
+	}
+
+	const handleOnClick = () => {
+		setToDos([...toDos, inputValue]);
+		setInputValue("");
+	}
+
+	const handleDelete = (index) => {
+		const updateToDos = toDos.filter((toDos, i) => i !== index)
+		setToDos(updateToDos);
+	}
+
 	return (
 		<div className="to-do">
 			<div className="wrapper">
 				<h1>todos</h1>
 				<div className="input-container">
-					<input type="text" />
-					<button className="add-btn">Add</button>
+					<input type="text" value={inputValue} onChange={handleOnChange}/>
+					<button className="add-btn" onClick={handleOnClick}>Add</button>
 				</div>
+				
 				<ul>
-					<li>Superar desafios<button className="delete-btn">Delete</button></li>
-					<p className="empty">What needs to be done?</p>
+					{toDos.length > 0 ? toDos.map((toDo, index) => {
+						return(
+							<li key={index}>{toDo}<button className="delete-btn" onClick={() => handleDelete(index)}>Delete</button></li>
+						)
+					})
+				 : <p>No tasks yet</p>}
 				</ul>
 			</div>
 			
